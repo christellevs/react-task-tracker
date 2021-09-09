d53029546f88c44e492315b0476cc1288ea93d8f
Christelle Van Sebroeck <christelle.marie@gmail.com>
2021-09-09 18:02:40 +0200

    added AddTask

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -25,6 +26,11 @@ function App() {
     }
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    console.log(task);
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -43,6 +49,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks}
         onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
